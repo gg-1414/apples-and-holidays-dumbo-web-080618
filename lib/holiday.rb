be7d1aace2e89920ecffb59b3c7860bc110df27b
Gina Lee <ginalee1114@gmail.com>
2018-07-29 05:01:09 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,6 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
       if holiday_name.length == 1 
         puts "  #{holiday_key.to_s.tr("_", " ").capitalize}: #{supplies.join(", ")}"
       else  
+         holiday.
         puts " #{holiday_name.join(" ").capitalize}: #{supplies.join(" ")}"
       end 
     end 
