28e6b954b66d99a4e3aa51d9442a0b7459cdb1c3
Gina Lee <ginalee1114@gmail.com>
2018-07-29 04:45:11 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |holiday_key, supplies|
-      puts "  #{holiday_key.to_s.split.tr("_", " ")}: #{supplies.join(", ")}"
+      puts "  #{holiday_key.to_s.tr("_", " ")}: #{supplies.join(", ")}"
 
     end 
   end 
